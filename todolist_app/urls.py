f0a8df8593c816ab2ecb5aab727914486d1ccdea
Abdul Kuddus <55076423+abdulkuddus225@users.noreply.github.com>
2019-11-14 10:25:58 +0530

    Add files via upload

---
+++ todolist_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.todolist,name="todolist"),
+    path('delete/<task_id>',views.delete_task,name="delete"),
+    path('complete/<task_id>',views.complete_task, name="complete"),
+    path('edit/<task_id>',views.edit_task,name="edit"),
+   
+]
